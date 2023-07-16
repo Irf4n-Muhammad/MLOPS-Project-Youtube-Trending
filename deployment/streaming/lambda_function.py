f32a59a49243b0f4f1ea5a7ae8f0a718be3e535a
Ubuntu <ubuntu@ip-172-31-12-75.ap-southeast-1.compute.internal>
2023-07-16 10:05:34 +0000

    last commit

--- deployment/streaming/lambda_function.py
+++ deployment/streaming/lambda_function.py
@@ -4,11 +4,11 @@
 
 
 {
-    "ride" : {
+    "Trending" : {
     'Likes': 50000000,
     'Views': 70000000,
     'Licensed': 'Yes',
     },
-    "ride_id": 123 
+    "Trending": 123 
 }
 
--- deployment/web-service-mlflow/test.py
+++ deployment/web-service-mlflow/test.py
@@ -1,13 +1,13 @@
 import requests
 
-ride = {
+Trending = {
     'Likes': 50000000,
     'Views': 70000000,
     'Licensed': 'Yes',
 }
 
 url = 'http://localhost:9696/predict'
-response = requests.post(url, json = ride)
+response = requests.post(url, json = Trending)
 print(response.json())
 
 
